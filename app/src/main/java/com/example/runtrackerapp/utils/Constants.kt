7f30f24e827f092b46d6b36cd100efa8d8ce7146
CyloTM <samuelchyke@hotmail.com>
2021-07-22 13:15:51 +0100

    Location Permissions

--- app/src/main/java/com/example/runtrackerapp/utils/Constants.kt
+++ app/src/main/java/com/example/runtrackerapp/utils/Constants.kt
@@ -3,5 +3,6 @@ package com.example.runtrackerapp.utils
 object Constants {
 
     const val RUNNING_DATABASE_NAME = "run_db"
+    const val REQUEST_CODE_LOCATION_PERMISSION = 0
 
 }
---
+++ app/src/main/java/com/example/runtrackerapp/utils/TrackingUtility.kt
@@ -0,0 +1,16 @@
+package com.example.runtrackerapp.utils
+
+import android.Manifest
+import android.content.Context
+import pub.devrel.easypermissions.EasyPermissions
+
+object TrackingUtility {
+
+    fun hasLocationPermissions(context: Context) =
+        EasyPermissions.hasPermissions(
+            context,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_BACKGROUND_LOCATION
+        )
+}
